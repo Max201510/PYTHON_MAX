d7be63bbcb3c8af78ee49fb95f057561af7a4ee1
Max201510 <lyndonqin@gmail.com>
2021-10-09 12:32:41 +1100

    first

---
+++ test.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import logging
+
+
+
+def test_1(a: int, b: int):
+    """[summary]
+
+    Args:
+        a (int): [description]
+        b (int): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    logging.warning(
+        f"This is info"
+        + f"from {a} to {b}"
+    )
+    return 1
+
+c = test_1(1, 2)
+print(c)
+logging.info("xxxx")
